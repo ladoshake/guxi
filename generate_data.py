feb92603bece595041775edd6681ef76521383df
ladoshake <ladoshake@hotmail.com>
2026-06-30 17:57:44 +0800

    更新时区

--- generate_data.py
+++ generate_data.py
@@ -2,6 +2,7 @@ import akshare as ak
 import pandas as pd
 import re
 from datetime import datetime, timedelta
+import pytz
 import json
 import logging
 import os
@@ -231,7 +232,7 @@ def get_stock_data():
         backup_data = load_backup_data()
         if backup_data:
             logger.info("使用备份数据更新时间戳后返回")
-            backup_data['update_time'] = (datetime.now() + timedelta(hours=8)).strftime("%Y-%m-%d %H:%M:%S")
+            backup_data['update_time'] = datetime.now(pytz.timezone('Asia/Shanghai')).strftime("%Y-%m-%d %H:%M:%S")
             backup_data['is_backup'] = True
             return backup_data
         else:
@@ -327,7 +328,7 @@ def get_stock_data():
     return {
         'ttm': results_ttm,
         'lfy': results_lfy,
-        'update_time': (datetime.now() + timedelta(hours=8)).strftime("%Y-%m-%d %H:%M:%S"),
+        'update_time': datetime.now(pytz.timezone('Asia/Shanghai')).strftime("%Y-%m-%d %H:%M:%S"),
         'is_backup': False
     }
 
